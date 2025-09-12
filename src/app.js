6ad1c70282c55ef33fef40aed1a7ee792f2ab250
Uday Kiran <s210540@rguktsklm.ac.in>
2025-09-12 21:15:20 +0530

    Database CRUD operation

--- src/app.js
+++ src/app.js
@@ -14,6 +14,47 @@ app.post("/signup",async(req,res)=>{
         res.status(400).send("error in user adding"+error.message);
     }
 });
+app.get("/feed",async(req,res)=>{
+    try{
+        const users=await User.find({});      
+        res.send(users);
+        console.log(users);
+    }
+    catch(error){ 
+        res.send("error");
+    }
+});
+app.get("/user",async(req,res)=>{
+    const userEmail=req.body.email;
+    try{
+        const users=await User.find({email:userEmail});      
+        res.send(users);
+    }
+    catch(error){
+        res.send("error");
+    }
+});
+app.delete("/deleteuser",async(req,res)=>{
+    const userId=req.body.userId;
+    try{
+        const users=await User.findByIdAndDelete(userId);      
+        res.send("user deleted");
+    }
+    catch(error){ 
+        res.send("error");
+    }
+});
+app.patch("/updateuser",async(req,res)=>{
+    const userId=req.body.userId;
+    const updateData=req.body;
+    try{
+        await User.findByIdAndUpdate({_id:userId},updateData);      
+        res.send("user updated");
+    }
+    catch(error){ 
+        res.send("error");
+    }
+});
 connectDB()
 .then(() => {
     console.log("DB connected successfully");
